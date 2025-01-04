0336ee5e44503fcf547b032dff9e9ac25ee57292
sultonmamadova <97739093+sultonmamadova@users.noreply.github.com>
2025-01-04 19:11:03 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,14 @@ import pandas as pd
 st.title('🎈 App Name')
 
 st.write('Hello world!')
+
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+with st.expander('Data'):
+  st.write("y")
+  x_raw=df.drop('species', axis=1)
+  st.dataframe(x_raw)
+
+  st.write("y")
+  y_raw=df.species
+  st.dataframe(y_raw)
+  
