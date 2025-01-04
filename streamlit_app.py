32887a6f4b8bbdb202b2511f18f9b5ffa61c8c42
sultonmamadova <97739093+sultonmamadova@users.noreply.github.com>
2025-01-04 19:26:36 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ with st.expander('Data'):
 
 with st.sidebar:
   st.header("Выбери:")
-   island = st.selectbox('Island', ('Torgersen', 'Dream', 'Biscoe'))
+  island = st.selectbox('Island', ('Torgersen', 'Dream', 'Biscoe'))
   bill_lenght_mm = st.slider('Bill length (mm)', 32.1, 59.6, 44.5)
   bill_lenght_mm = st.slider('Bill length (mm)', 13.1, 21.5, 17.3)
   flipper_length_mm = st.slider('Flipper length (mm)', 32.1, 59.6, 44.5)
